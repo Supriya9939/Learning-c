c579f4f434bbad379bea10e5bb0e53fb58f6105e
Supriya Mandal <supriyomondal830@gmail.com>
2024-03-15 20:53:46 +0530

    my 4th code

---
+++ hackerrank/practice/challenges/challenges_pointer_in.c
@@ -0,0 +1,22 @@
+/*In this challenge, you will learn to implement the basic functionalities of pointers in C. A pointer in C is a way to share a memory address among different contexts (primarily functions). They are primarily used whenever a function needs to modify the content of a variable that it does not own.
+Complete the function void update(int *a,int *b). It receives two integer pointers, int* a and int* b. Set the value of  to their sum, and  to their absolute difference. There is no return value, and no return statement is needed.
+*/
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    *a=*a+*b;
+    *b=*a-*b-*b;
+    if(*b<0)
+    *b=*b*(-1);
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
