db88021c0d9d61b6afa50e0380530de20e231bb7
Supriya Mandal <supriyomondal830@gmail.com>
2024-03-17 11:36:21 +0530

    my 3rd code

---
+++ codechef/course/course_c_LBCL01_problems_CLB003.c
@@ -0,0 +1,17 @@
+/*Printing text
+In the last problem, we saw how to print a number(using printf()).
+
+To print a number we write this:
+
+printf("%d", 42);
+But to output a text, we do this
+
+printf("I love C");
+This will print I love C to the screen. No need to add any %d or anything. Just the text you want to print inside quotes ("").*/
+#include <stdio.h>
+
+int main() {
+
+  printf("I Love C");
+
+}
