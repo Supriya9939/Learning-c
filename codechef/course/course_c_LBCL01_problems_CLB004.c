cd1c51eb1872509f4d17c1ab50f3c6dcf4342d9a
Supriya Mandal <supriyomondal830@gmail.com>
2024-03-17 11:36:34 +0530

    my 4th code

---
+++ codechef/course/course_c_LBCL01_problems_CLB004.c
@@ -0,0 +1,26 @@
+/*Arithmetic Operations
+We can also perform mathematical operations (like addition, subtraction etc) with printf().
+
+These use the familiar symbols of:
+
++
++ for addition
+−
+− for subtraction
+∗
+∗ for multiplication
+/
+/ for division
+To add two number we do this:
+
+printf("%d", 21 + 40);
+The above line of code will output: 61*/
+
+#include <stdio.h>
+
+int main() {
+
+  // Replace the underscores
+  printf("%d",21+40);
+
+}
